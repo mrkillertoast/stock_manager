a4beca3b86d2a1a92b6e1719527f37bd630b5eaa
mrkillertoast <lueku.rauscher@gmail.com>
2024-10-29 08:14:57 +0100

    - added nuxt runtimeconfig

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,24 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-				compatibilityDate: '2024-04-03',
-				devtools: { enabled: true },
-				modules: [ '@nuxtjs/tailwindcss', 'shadcn-nuxt' ],
-				shadcn: {
-								/**
-									* Prefix for all the imported component
-									*/
-								prefix: '',
-								/**
-									* Directory that the component lives in.
-									* @default "./components/ui"
-									*/
-								componentDir: './components/ui'
-				},
-				ssr: false,
-				runtimeConfig: {
-								public: {
-												endpoint: process.env.APPWRITE_ENDPOINT,
-												projectID: process.env.APPWRITE_PROJECT_ID,
-								}
-				},
+	compatibilityDate: '2024-04-03',
+	devtools: { enabled: true },
+	modules: [ '@nuxtjs/tailwindcss', 'shadcn-nuxt' ],
+	shadcn: {
+		/**
+		 * Prefix for all the imported component
+		 */
+		prefix: '',
+		/**
+		 * Directory that the component lives in.
+		 * @default "./components/ui"
+		 */
+		componentDir: './components/ui'
+	},
+	ssr: false,
+	runtimeConfig: {
+		public: {
+			appwriteEndpoint: process.env.APPWRITE_ENDPOINT,
+			appwriteProjectId: process.env.APPWRITE_PROJECT_ID,
+		}
+	},
 })
