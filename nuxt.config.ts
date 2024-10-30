43cb5c9b63e0bc490e75dcdec7b78cd260503a8b
mrkillertoast <lueku.rauscher@gmail.com>
2024-10-30 19:45:44 +0100

    removed localtunnel added nuxthub package

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,32 +1,29 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	compatibilityDate: '2024-04-03',
-	devtools: { enabled: true },
-	modules: [
-		'@nuxtjs/tailwindcss',
-		'shadcn-nuxt',
-		'@nuxt/icon',
-		'@nuxtus/nuxt-localtunnel',
-	],
-	shadcn: {
-		/**
-		 * Prefix for all the imported component
-		 */
-		prefix: '',
-		/**
-		 * Directory that the component lives in.
-		 * @default "./components/ui"
-		 */
-		componentDir: './components/ui'
-	},
-	ssr: false,
-	runtimeConfig: {
-		public: {
-			appwriteEndpoint: process.env.APPWRITE_ENDPOINT,
-			appwriteProjectId: process.env.APPWRITE_PROJECT_ID,
-		}
-	},
-	localtunnel:{
-		local_https: true
-	}
+				compatibilityDate: '2024-04-03',
+				devtools: { enabled: true },
+				modules: [
+				 '@nuxtjs/tailwindcss',
+				 'shadcn-nuxt',
+				 '@nuxt/icon',
+				 '@nuxthub/core',
+				],
+				shadcn: {
+								/**
+									* Prefix for all the imported component
+									*/
+								prefix: '',
+								/**
+									* Directory that the component lives in.
+									* @default "./components/ui"
+									*/
+								componentDir: './components/ui'
+				},
+				ssr: false,
+				runtimeConfig: {
+								public: {
+												appwriteEndpoint: process.env.APPWRITE_ENDPOINT,
+												appwriteProjectId: process.env.APPWRITE_PROJECT_ID,
+								}
+				},
 })
