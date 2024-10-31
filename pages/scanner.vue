118b3483c28ba42dcd7ac85259098e7d21d2ee8e
mrkillertoast <lueku.rauscher@gmail.com>
2024-10-31 17:00:38 +0100

    Added sections

--- pages/scanner.vue
+++ pages/scanner.vue
@@ -23,30 +23,31 @@ function toggleScan() {
 
 <template>
   <div class="scanner-page mt-5 grid place-items-center gap-5">
-    <div class="camera-header h-12 grid grid-cols-4 place-items-center w-[300px]">
+    <section class="camera-header h-12 grid grid-cols-4 place-items-center w-[300px]">
       <div class="col-span-3 justify-self-start text-2xl font-bold">
         Scan a Barcode
       </div>
       <div class="col justify-self-end">
         <Button size="icon" @click="toggleScan">X</Button>
       </div>
-    </div>
-    <ClientOnly>
-      <code-scanner
-          :qrbox="200"
-          :fps="10"
-          style="width: 300px;height: 300px;"
-          @result="onScan"
-          v-if="showScanner"
-      />
-      <div
-          class="scanner-placeholder rounded-xl shadow-2xl bg-gray-400 text-center text-2xl grid place-items-center p-3"
-          v-else>
-        {{ scanResult }}
-      </div>
-    </ClientOnly>
-    <div class="article-options grid gap-4 p-4 rounded-xl shadow-2xl bg-neutral-100">
-
+    </section>
+    <section class="camera-body">
+      <ClientOnly>
+        <code-scanner
+            :qrbox="200"
+            :fps="10"
+            style="width: 300px;height: 300px;"
+            @result="onScan"
+            v-if="showScanner"
+        />
+        <div
+            class="scanner-placeholder rounded-xl shadow-2xl bg-gray-400 text-center text-2xl grid place-items-center p-3"
+            v-else>
+          {{ scanResult }}
+        </div>
+      </ClientOnly>
+    </section>
+    <section class="article-options grid gap-4 p-4 rounded-xl shadow-2xl bg-neutral-100">
       <div class="mode-switcher">
         <Tabs default-value="add" class="w-[268px] shadow rounded-xl">
           <TabsList class="w-full">
@@ -68,9 +69,8 @@ function toggleScan() {
       <div class="save-container ">
         <Button class="w-full">Save Item</Button>
       </div>
-    </div>
+    </section>
   </div>
-
 </template>
 
 <style scoped>
