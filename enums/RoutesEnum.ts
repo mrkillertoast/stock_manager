7753ad83e0cf58fe2041007571ddb0b15e61f3e3
mrkillertoast <lueku.rauscher@gmail.com>
2024-11-01 17:38:39 +0100

    Updated Routes - fixed route Enum

--- enums/RoutesEnum.ts
+++ enums/RoutesEnum.ts
@@ -1,5 +1,5 @@
 export enum Routes {
-	OVERVIEW = 'stock-overview',
-	SCANNER = 'scanner',
-	NEW_ITEM = 'new-product'
+	OVERVIEW = '/stock-overview',
+	SCANNER = '/scanner',
+	NEW_ITEM = '/new-product'
 }
