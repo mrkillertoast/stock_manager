8f16464126cea05bc7c6a1cb0925391ed86b2aca
mrkillertoast <lueku.rauscher@gmail.com>
2024-11-01 12:36:35 +0100

    Updated Toast - added success type with green background

--- components/ui/toast/index.ts
+++ components/ui/toast/index.ts
@@ -21,6 +21,7 @@ export const toastVariants = cva(
         default: 'border bg-background text-foreground',
         destructive:
                     'destructive group border-destructive bg-destructive text-destructive-foreground',
+          success: 'border bg-green-200 text-foreground'
       },
     },
     defaultVariants: {
