fe21c2550b31f605a4f7270868cedfe5e2fed524
mrkillertoast <lueku.rauscher@gmail.com>
2024-11-18 17:18:56 +0100

    Recipe Generation Page Layout - add new components - create basic layout

---
+++ components/ui/ThreeOptionSwitch.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group'
+import { Label } from '@/components/ui/label'
+
+const props = defineProps({
+  options: {
+    type: Array<string>,
+    required: true,
+    default: ['Option1','Option2','Option3']
+  }
+})
+const selected = ref('option1')
+</script>
+
+<template>
+  <RadioGroup v-model="selected" class="custom-switch">
+    <div class="flex space-x-1 p-1 bg-gray-200 rounded-full">
+      <Label
+          v-for="option in props.options"
+          :key="props.options.indexOf(option)"
+          :for="option"
+          class="flex items-center justify-center w-1/3 py-2 rounded-full cursor-pointer transition-colors"
+          :class="{ 'bg-white shadow-sm': selected === option }"
+      >
+        <RadioGroupItem :value="option" :id="option" class="sr-only"/>
+        {{ option }}
+      </Label>
+    </div>
+  </RadioGroup>
+</template>
+
+<style scoped>
+.custom-switch {
+  display: inline-block;
+  ;
+}
+</style>
--- pages/recipe-generation.vue
+++ pages/recipe-generation.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import type { Html5QrcodeResult } from "html5-qrcode/core";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 import {
   NumberField,
@@ -8,167 +7,86 @@ import {
   NumberFieldIncrement,
   NumberFieldInput,
 } from '@/components/ui/number-field'
-import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
-import findEanInDB from "~/utils/database/findEanInDB";
-import updateDocument from "~/utils/database/updateDocument";
-import { useToast } from "~/components/ui/toast";
 import PageHeader from "~/components/PageHeader.vue";
-import { Routes } from "~/enums/RoutesEnum";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "~/components/ui/card";
+import ThreeOptionSwitch from "~/components/ui/ThreeOptionSwitch.vue";
 
-definePageMeta({
-  middleware: [ 'auth' ],
-  requiresAuth: true
-});
+const mode = ref('new')
+const portionSize = ref(2)
+const cookingTime = ref(30)
 
-const { toast } = useToast()
-const router = useRouter()
-
-const scanResult = ref('')
-const tabAddRemove = ref('add')
-const newAmount = ref(0)
-
-async function onScan(decodedText: string, decodedResult: Html5QrcodeResult) {
-  scanResult.value = decodedText
-}
-
-async function handleData() {
-  const res = await findEanInDB(scanResult.value)
-
-  if (res?.total === 0) {
-    await router.push(`${ Routes.NEW_ITEM }/${ scanResult.value }`)
-    return
-  }
-
-  if (res?.total > 1) {
-    toast({
-      title: 'Error',
-      description: `Database contains more than 1 document with this EAN Code`,
-      variant: "destructive"
-    })
-    return
-  }
-
-  const document = res.documents[ 0 ];
-
-  let amount = document.amount
-  switch (tabAddRemove.value) {
-    case 'add':
-      amount = amount + newAmount.value
-      break
-    case 'remove':
-      amount = amount - newAmount.value
-      break
-  }
-
-  if (amount < 0) {
-    amount = 0
-    toast({
-      title: 'Amount Changed',
-      description: `The amount was changed to 0`,
-      variant: "default"
-    })
-  }
-
-  try {
-    await updateDocument(document.$id, { 'amount': amount })
-        .then((res) => {
-          console.log(res)
-          toast({
-            title: 'Change successful',
-            description: `Database was updated new amount ${ res.amount }`,
-            variant: "success"
-          })
-        });
-  } catch (error) {
-    console.log(error)
-    toast({
-      title: 'ERROR',
-      description: `${ error }`,
-      variant: "destructive"
-    })
-  }
-}
-
+//Cooking Experience Handling
+const cookingExperienceOptions = [ 'Easy', 'Medium', 'Hard' ]
+const cookingExperience = ref(0)
 
 </script>
 
 <template>
   <div class="scanner-page grid place-items-center gap-0 variable-container">
-    <PageHeader title="Scan Barcode"/>
-    <section class="camera-body">
-      <ClientOnly>
-        <Tabs default-value="camera" class="w-[300px] shadow rounded-xl">
-          <TabsList class="w-full">
-            <TabsTrigger value="camera" class="w-full">
-              Camera
-            </TabsTrigger>
-            <TabsTrigger value="manual" class="w-full">
-              Manual
-            </TabsTrigger>
-          </TabsList>
-          <TabsContent value="camera">
-            <code-scanner
-                :qrbox="150"
-                :fps="10"
-                style="width: 300px;height: 300px;"
-                @result="onScan"
-            />
-          </TabsContent>
-          <TabsContent value="manual">
-            <div
-                class="scanner-placeholder rounded-xl shadow-2xl bg-gray-400 text-center text-2xl grid place-items-center p-3">
-              <div class="manual-search">
-                <Label for="ean-search" class="align-self-start">EAN-Search</Label>
-                <Input id="ean-search" type="text" placeholder="Enter EAN-Number" v-model="scanResult"/>
-              </div>
+    <PageHeader title="Recipe Generation"/>
+    <Card class="bg-neutral-100 shadow-2xl w-96 p-4">
+      <CardHeader>
+        <CardTitle>Recipe Generation</CardTitle>
+        <CardDescription>What do we create today?</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <div class="article-options">
+          <div class="mode-switcher">
+            <div class="ean-code">
+              <Label for="found-ean" class="align-self-start">Generation Mode</Label>
+              <Tabs v-model="mode" class="shadow rounded-xl">
+                <TabsList class="w-full">
+                  <TabsTrigger value="new" class="w-full">
+                    New Recipe
+                  </TabsTrigger>
+                  <TabsTrigger value="stock" class="w-full">
+                    Create from stock
+                  </TabsTrigger>
+                </TabsList>
+              </Tabs>
             </div>
-          </TabsContent>
-        </Tabs>
-      </ClientOnly>
-    </section>
-    <section class="article-options grid gap-4 p-4 rounded-xl shadow-2xl bg-neutral-100">
-      <div class="mode-switcher">
-        <div class="ean-code">
-          <Label for="found-ean" class="align-self-start">EAN-Code</Label>
-          <Input disabled id="found-ean" type="text" placeholder="Enter code or scan using cam" v-model="scanResult"/>
+          </div>
         </div>
-        <br>
-        <Tabs v-model="tabAddRemove" class="w-[268px] shadow rounded-xl">
-          <TabsList class="w-full">
-            <TabsTrigger value="add" class="w-full">
-              Add
-            </TabsTrigger>
-            <TabsTrigger value="remove" class="w-full">
-              Remove
-            </TabsTrigger>
-          </TabsList>
-        </Tabs>
-      </div>
-      <div class="amount-selector">
-        <NumberField id="amount" :default-value="0" v-model="newAmount">
-          <Label for="amount">Amount</Label>
+        <NumberField id="amount" :default-value="0" v-model="portionSize" class="gird grid-cols-2 items-center mt-3">
+          <Label for="amount">Desired portion-size</Label>
           <NumberFieldContent>
             <NumberFieldDecrement/>
             <NumberFieldInput/>
             <NumberFieldIncrement/>
           </NumberFieldContent>
         </NumberField>
-      </div>
-      <div class="save-container ">
-        <Button class="w-full" @click="handleData">Save Item</Button>
-      </div>
-    </section>
+        <NumberField id="amount" :default-value="0" v-model="cookingTime" class="gird grid-cols-2 items-center mt-2">
+          <Label for="amount">Desired Cooking Time (min)</Label>
+          <NumberFieldContent>
+            <NumberFieldDecrement/>
+            <NumberFieldInput/>
+            <NumberFieldIncrement/>
+          </NumberFieldContent>
+        </NumberField>
+        <div class="flex flex-col mt-2">
+          <Label for="cooking-experience">Cooking Experience</Label>
+          <ThreeOptionSwitch id="cooking-experience" :options="cookingExperienceOptions" v-model="cookingExperience"/>
+        </div>
+        <div class="excluded-nutrients mt-2 flex flex-col">
+          <Label for="cooking-experience">Select Diet</Label>
+          <p>Placeholder</p>
+        </div>
+        <div class="additional-prompt">
+          <Label for="additional-notes">Additional notes / remarques </Label>
+          <Textarea id="additional-notes" class="w-full rounded-2xl p-1 text-sm"
+                    placeholder="ex. I would like something fruity."/>
+        </div>
+      </CardContent>
+      <CardFooter>
+        <Button class="w-full">Create Recipe</Button>
+      </CardFooter>
+    </Card>
   </div>
   <FooterNavigation/>
 </template>
 
 <style scoped>
-.scanner-placeholder {
-  width: 300px;
-  height: 400px;
-}
-
 .variable-container {
   height: calc(100dvh - 4rem);
   overflow: auto;
